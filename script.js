dd9624c02673c0a222ebe8ef3879f304789ca5b2
Pranav Ausekar <104610715+AusekarPranav@users.noreply.github.com>
2024-03-14 13:56:00 +0530

    changes done

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 
 // alert("hey, pranav");
 
-function myFunction() {
+// function myFunction() {
+//     document.querySelector("#one").style.backgroundColor = "red";
+// }
+// function mySecondFunction() {
+//     document.querySelector("#two").style.backgroundColor = "blue";
+// }
+// function myThirdFunction() {
+//     document.querySelector("#three").style.backgroundColor = "pink";
+// }
+// function myFourthFunction() {
+//     document.querySelector("#four").style.backgroundColor = "yellow";
+// }
+// function myFifthFunction() {
+//     document.querySelector("#five").style.backgroundColor = "white";
+// }
+
+
+document.querySelector("#one").addEventListener('click', () => {
     document.querySelector("#one").style.backgroundColor = "red";
-}
+})
 
-function mySecondFunction() {
+document.querySelector("#two").addEventListener('click', () => {
     document.querySelector("#two").style.backgroundColor = "blue";
-}
+})
 
-function myThirdFunction() {
+document.querySelector("#three").addEventListener('click', () => {
     document.querySelector("#three").style.backgroundColor = "pink";
-}
+})
 
-function myFourthFunction() {
+document.querySelector("#four").addEventListener('click', () => {
     document.querySelector("#four").style.backgroundColor = "yellow";
-}
+})
 
-function myFifthFunction() {
+document.querySelector("#five").addEventListener('click', () => {
     document.querySelector("#five").style.backgroundColor = "white";
-}
+})
