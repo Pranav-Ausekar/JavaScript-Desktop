8f74aa99582fedfc26b21a22002a72b497d22eaf
Pranav Ausekar <104610715+AusekarPranav@users.noreply.github.com>
2024-03-14 10:36:50 +0530

    Events in JS

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// alert("hey, pranav");
+
+function myFunction() {
+    document.querySelector("#one").style.backgroundColor = "red";
+}
+
+function mySecondFunction() {
+    document.querySelector("#two").style.backgroundColor = "blue";
+}
+
+function myThirdFunction() {
+    document.querySelector("#three").style.backgroundColor = "pink";
+}
+
+function myFourthFunction() {
+    document.querySelector("#four").style.backgroundColor = "yellow";
+}
+
+function myFifthFunction() {
+    document.querySelector("#five").style.backgroundColor = "white";
+}
